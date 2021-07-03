80330982c95e0375c296b123f01a2bcaedc056dc
yuanyi.he <yuanyi.he@qq.com>
2021-07-03 10:41:40 +0800

    完成P5739

---
+++ function/P5739.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    int sum=1;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        sum=sum*i;
+    }
+    cout<<sum<<endl;
+    return 0;
+}
